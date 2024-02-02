8e7ca989e52a12a80b4c5efaad7cbde58382afe2
vanshita-tilwani <msvtilwani@gmail.com>
2024-02-02 16:34:47 -0500

    Pre-processing documents : parsing all the docs

--- source/assignment1.py
+++ source/assignment1.py
@@ -11,3 +11,31 @@ def getStopwords() :
         content = f.read()
         names = content.split("\n")
     return names
+
+# Parse all the documents from the given content
+def parseDocuments(content: str) :
+    pattern = '(?s)(?<=<DOC>)(.*?)(?=</DOC>)'
+    return re.findall(pattern, content)
+
+# Parse the document ID from the document
+def parseDocumentID(document: str) :
+    pattern = '(?s)(?<=<DOCNO>)(.*?)(?=</DOCNO>)'
+    return re.search(pattern, document).group().strip()
+
+# Parse the document text from the document
+def parseDocumentText(document: str) :
+    pattern = '(?s)(?<=<TEXT>)(.*?)(?=</TEXT>)'
+    return re.search(pattern, document).group().strip()
+
+allDocuments = {}
+# Read all the files documents to be indexed from the mentioned directory
+allFiles = "/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/IR_data/IR_data/AP_DATA/ap89_collection"
+for filename in os.listdir(allFiles):
+    with open(os.path.join(allFiles, filename), 'rb') as f:
+        content = f.read().decode("iso-8859-1")
+        documents = parseDocuments(content)
+        for document in documents:
+            docID = parseDocumentID(document)
+            docText = parseDocumentText(document)
+            allDocuments[docID] = docText
+print('Done parsing documents')
