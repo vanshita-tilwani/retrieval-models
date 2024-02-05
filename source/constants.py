5463197ca47aeb8410c25e539f4741f8b1af79d4
vanshita-tilwani <msvtilwani@gmail.com>
2024-02-05 18:07:02 -0500

    Updating  Path of data files

--- source/constants.py
+++ source/constants.py
@@ -1,8 +1,8 @@
 class Constants:
-    DATA_PATH = '/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/IR_data/IR_data/AP_DATA/ap89_collection'
+    DATA_PATH = '/Users/vanshitatilwani/Documents/Courses_Local/hw1-vanshita-tilwani/IR_data/AP_DATA/ap89_collection'
     DOC_TYPE = 'document'
     INDEX_NAME = 'ap89_data1'
     MAX_OUTPUT = 1000
-    RESULTS_PATH = '/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/trec_eval/'
-    STOPWORDS_PATH = '/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/IR_data//AP_DATA/stoplist.txt'
-    QUERY_PATH = '/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/IR_data/AP_DATA/query_desc.51-100.short.txt'
+    RESULTS_PATH = '/Users/vanshitatilwani/Documents/Courses_Local/CS6200/hw1-vanshita-tilwani/trec_eval/'
+    STOPWORDS_PATH = '/Users/vanshitatilwani/Documents/Courses_Local/CS6200/hw1-vanshita-tilwani/IR_data//AP_DATA/stoplist.txt'
+    QUERY_PATH = '/Users/vanshitatilwani/Documents/Courses_Local/CS6200/hw1-vanshita-tilwani/IR_data/AP_DATA/query_desc.51-100.short.txt'
