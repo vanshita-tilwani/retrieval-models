1065e0f6d2859aec2fbf171b2bbb9d420047beeb
vanshita-tilwani <msvtilwani@gmail.com>
2024-02-05 01:30:01 -0500

    changes to retrieval models

--- source/assignment1.py
+++ source/assignment1.py
@@ -1,6 +1,8 @@
 import os
 import re
 from elasticsearch7 import Elasticsearch
+from concurrent.futures import ThreadPoolExecutor
+from collections import OrderedDict
 
 
 # Region 1 - Text Pre - Processing i.e. fetching documents, stopwords and parsing the data
@@ -112,65 +114,26 @@ def addData(indexName, docID, text) :
 # Region 3 - 
 
 # Execute queries and retrieve results
-def execute_queries(index):
-
-    # Read queries from query file
-    with open('/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/IR_data/IR_data/AP_DATA/query_desc.51-100.short.txt', 'r') as query_file:
-        queries = query_file.readlines()
-
-    for query in queries:
-        # Parse the query and extract query number and text
-        query_number, query_text = query.split(' ', 1)
-        
-        esbuiltResponse = ES_search(index, query_text)
-        
-        with open(f'esbuiltin_results_{query_number}.txt', 'w') as output_file:
-            for idx, hit in enumerate(esbuiltResponse['hits']['hits']):
-                docno = hit['_id']
-                score = hit['_score']
-                output_file.write(f"{query_number} Q0 {docno} {idx+1} {score} Exp\n")
-
-        okapiTFResponse = OkapiTF(index, query_text)
-
-        with open(f'okapitf_results_{query_number}.txt', 'w') as output_file:
-            for idx, hit in enumerate(okapiTFResponse['hits']['hits']):
-                docno = hit['_id']
-                score = hit['_score']
-                output_file.write(f"{query_number} Q0 {docno} {idx+1} {score} Exp\n")
-        
-        TfidfResponse = TFIDF(index, query_text)
-        with open(f'tfidf_results_{query_number}.txt', 'w') as output_file:
-            for idx, hit in enumerate(TfidfResponse['hits']['hits']):
-                docno = hit['_id']
-                score = hit['_score']
-                output_file.write(f"{query_number} Q0 {docno} {idx+1} {score} Exp\n")
-
-def fetchTermVectors() :
-    term_vectors = {}
-    for document in list(documents.keys()):
-        body = {
-            'ids' : [document],
-            'parameters' : {
-                'fields' : ['content'],
-                'field_statistics' : True,
-                'term_statistics' : True
-            }
-        }
-        term_vectors[document] = es.mtermvectors(index= index, body=body)
-    return term_vectors
 
 def ES_search(query) :
     return es.search(index=index, query={'match' : {'content' : query}}, size=1000)
 
 def OkapiTF(query) :
-    term_vector = fetchTermVectors()
-    scores = {}
+    scores= OrderedDict()
+    averageLength = word_count['total']/len(documents)
     for document in documents:
         score = 0
         # TODO : check if this works
-        length = len(documents[document])
+        length = word_count[document]
         for word in query.split(" "):
-            print('Hi')
+            if(word in term_vectors[document]):
+                tf = term_vectors[document][word]['term_freq']
+                denominator = tf + 0.5 + 1.5 * (length/averageLength)
+                currentScore = tf/denominator
+                score+= currentScore
+        scores[document] = score
+    return scores
+
 
 def TFIDF(indexName, query):
     return es.search(index=indexName, body={
@@ -188,6 +151,59 @@ def TFIDF(indexName, query):
             "_source": False
         })
 
+
+#Utils
+
+def fetch_term_vectors(document):
+    body = {
+        'ids': [document],
+        'parameters': {
+            'fields': ['content'],
+            'field_statistics': True,
+            'term_statistics': True
+        }
+    }
+    term_vector = es.mtermvectors(index=index, body=body)
+    if 'content' in term_vector['docs'][0]['term_vectors']:
+        return document, term_vector['docs'][0]['term_vectors']['content']['terms']
+    else:
+        return document, {}
+    
+def fetchTermVectors() :
+    term_vectors = {}
+    with ThreadPoolExecutor() as executor:
+        # Submit tasks to fetch term vectors for each document
+        futures = {executor.submit(fetch_term_vectors, document): document for document in documents}
+        # Retrieve results
+        for future in futures:
+            document, term_vector = future.result()
+            term_vectors[document] = term_vector
+    return term_vectors
+    
+def analyze_text(document_text):
+    try:
+        tokens = es.indices.analyze(body={"text": document_text, "analyzer": "standard"})
+        return len(tokens['tokens'])
+    except Exception as e:
+        print(f"Error analyzing text: {e}")
+        return 0
+    
+def fetchWordCount() :
+    word_count = {}
+    total_word_count = 0
+    docs = es.mget(index=index, body={"ids": list(documents.keys())})
+    with ThreadPoolExecutor() as executor:
+        # Submit tasks to analyze text for each document
+        futures = {executor.submit(analyze_text, doc['_source']['content']): doc['_id'] for doc in docs['docs']}
+        for future in futures:
+            document_id = futures[future]
+            try:
+                word_count[document_id] = future.result()
+                total_word_count += word_count[document_id]
+            except Exception as e:
+                print(f"Error processing document {document_id}: {e}")
+    word_count['total'] = total_word_count
+    return word_count
 # Main Program 
 
 # Elastic Search Client and Index Name
@@ -198,7 +214,10 @@ documents = fetchDocuments()
 #print("Index with name : {index} has been created in Elastic Search")
 #indexDocuments()
 #print("All the documents have been added to the elasticsearch index named {index}")
+
 queries = fetchQueries()
+
+
 # ES Built In
 for query in queries :
     esbuiltResponse = ES_search(queries[query])
@@ -208,3 +227,17 @@ for query in queries :
             docno = hit['_id']
             score = hit['_score']
             output_file.write(f"{query} Q0 {docno} {idx+1} {score} Exp\n")
+
+term_vectors  = fetchTermVectors()
+word_count = fetchWordCount()
+
+# Okapi TF
+for query in queries :
+    scores = OkapiTF(queries[query])
+    sortedScores = OrderedDict(sorted(scores.items(), key=lambda x: x[1]))
+    okapiTfResponse = list(sortedScores.items())[::-1][:1000]
+    with open(f'/Users/vanshitatilwani/Documents/Courses/CS6200/hw1-vanshita-tilwani/trec_eval/okapitf_results.txt', 'a') as output_file:
+        for index, (document, score)in enumerate(okapiTfResponse):
+            output_file.write(f"{query} Q0 {document} {index+1} {score} Exp\n")
+
+    print('Hi')
